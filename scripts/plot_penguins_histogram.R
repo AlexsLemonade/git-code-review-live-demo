db67b0a46894ed7787f99488d1450a785c14e4f1
Stephanie <stephanie.spielman@gmail.com>
2023-07-28 13:38:22 -0400

    add script comments

--- scripts/plot_penguins_histogram.R
+++ scripts/plot_penguins_histogram.R
@@ -1,6 +1,8 @@
+# Load libraries ---------
 library(ggplot2)
 library(palmerpenguins)
 
+# Make histogram of penguin bill depth --------
 ggplot(penguins) +
   aes(x = bill_depth_mm) +
   geom_histogram() +
